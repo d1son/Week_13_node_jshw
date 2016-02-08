2e4253fe349235e19fcb41fb50bd5cdfc5862c98
d1son <dson120@gmail.com>
2016-02-08 16:56:47 -0500

    added some server js, linked html pages

--- server.js
+++ server.js
@@ -1,5 +1,22 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
-var PORT = 8080
+var PORT = process.env.PORT || 8080;
 
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get("/login", function(req, res) {
+	res.sendFile(process.cwd() + "views/portfolio-new-main.html");
+});
+
+app.get("/help", function(req, res) {
+	res.sendFile(process.cwd() + "views/rps-main.html");
+});
+
+app.get("/help", function(req, res) {
+	res.sendFile(process.cwd() + "views/clickyGame-home.html");;
+});
+
+app.listen(PORT, function() {
+	console.log("App is listening on port %s", PORT);
+});
