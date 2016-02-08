b4f86368548d5742a71e40c1fc14fce79d510663
d1son <dson120@gmail.com>
2016-02-08 18:18:52 -0500

    cleaned up a little code on server.js

--- server.js
+++ server.js
@@ -1,31 +1,27 @@
-// having a lot of troube understanding express and how to use node as a web server
+// having a lot of troube understanding express and how to use node as a web server, and uploading it to heroku
 
 var express = require("express");
-var bodyParser = require("body-parser");
+// var bodyParser = require("body-parser");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
 
 app.use("/js", express.static("public/js"));
-
+app.use("/images", express.static("public/images"));
 app.use("/css", express.static("public/css"));
 
 app.get("/", function(req, res) {
   res.sendFile(process.cwd() + "/views/portfolio-new-main.html");
 });
 
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.get("/portfolio", function(req, res) {
-	res.sendFile(process.cwd() + "views/portfolio-new-main.html");
-});
+// app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get("/rps", function(req, res) {
-	res.sendFile(process.cwd() + "views/rps-main.html");
+	res.sendFile(process.cwd() + "/views/rps-main.html");
 });
 
 app.get("/clickyGame", function(req, res) {
-	res.sendFile(process.cwd() + "views/clickyGame-home.html");;
+	res.sendFile(process.cwd() + "/views/clickyGame-home.html");;
 });
 
 app.listen(PORT, function() {
